0d6db4722a4e8bd4f3ea829e78aee22e513ed0b3
Murat Goktas <mcs.goktas@gmail.com>
2022-05-08 19:35:00 +0300

    Mg-06) @ToString, @Data, @Column , @GeneratedValue(strategy = GenerationType.IDENTITY) added.

--- CvPro/CvPro/src/main/java/com/CvProJava/CvPro/Entities/PersonelInformation.java
+++ CvPro/CvPro/src/main/java/com/CvProJava/CvPro/Entities/PersonelInformation.java
@@ -1,20 +1,27 @@
 package com.CvProJava.CvPro.Entities;
 
 import java.util.Date;
-import lombok.Getter;
-import lombok.Setter;
+
+
+
 import javax.persistence.*;
+
+
+import lombok.Data;
+import lombok.ToString;
 
 @Entity
 @Table(name="PersonelInformations")
-public class PersonelInformation {
-	
+@Data
+@ToString
+public class PersonelInformation {	
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name="Id")
 	private int Id;
-	@Getter @Setter
+	@Column(name="FirstName")
 	private String FirstName;
+	@Column(name="MiddleName")
 	private String MiddleName;
 	private String LastName;
 	private Date  Birthdate;
@@ -23,75 +30,14 @@ public class PersonelInformation {
 	private boolean MilitaryStatus;
 	private String PhotoLink;
 	private Date MilitaryEndDate;
-	public PersonelInformation(int id, String firstName, String middleName, String lastName, Date birthdate,
-			String birthCity, boolean maritalStatus, boolean militaryStatus, String photoLink, Date militaryEndDate) {
-		super();
-		Id = id;
-		FirstName = firstName;
-		MiddleName = middleName;
-		LastName = lastName;
-		Birthdate = birthdate;
-		BirthCity = birthCity;
-		MaritalStatus = maritalStatus;
-		MilitaryStatus = militaryStatus;
-		PhotoLink = photoLink;
-		MilitaryEndDate = militaryEndDate;
-	}
-	public int getId() {
-		return Id;
-	}
-	public void setId(int id) {
-		Id = id;
-	}
-	 
-	public String getMiddleName() {
-		return MiddleName;
-	}
-	public void setMiddleName(String middleName) {
-		MiddleName = middleName;
-	}
-	public String getLastName() {
-		return LastName;
-	}
-	public void setLastName(String lastName) {
-		LastName = lastName;
-	}
-	public Date getBirthdate() {
-		return Birthdate;
-	}
-	public void setBirthdate(Date birthdate) {
-		Birthdate = birthdate;
-	}
-	public String getBirthCity() {
-		return BirthCity;
-	}
-	public void setBirthCity(String birthCity) {
-		BirthCity = birthCity;
-	}
-	public boolean isMaritalStatus() {
-		return MaritalStatus;
-	}
-	public void setMaritalStatus(boolean maritalStatus) {
-		MaritalStatus = maritalStatus;
-	}
-	public boolean isMilitaryStatus() {
-		return MilitaryStatus;
-	}
-	public void setMilitaryStatus(boolean militaryStatus) {
-		MilitaryStatus = militaryStatus;
-	}
-	public String getPhotoLink() {
-		return PhotoLink;
-	}
-	public void setPhotoLink(String photoLink) {
-		PhotoLink = photoLink;
-	}
-	public Date getMilitaryEndDate() {
-		return MilitaryEndDate;
-	}
-	public void setMilitaryEndDate(Date militaryEndDate) {
-		MilitaryEndDate = militaryEndDate;
+	@ToString.Include
+	String FullName(){
+		if (MiddleName=="") {
+			return FirstName+" "+LastName;
+		}else {
+			return FirstName+" "+MiddleName+" "+LastName;
+		}
 	}
 	
-	
+
 }
